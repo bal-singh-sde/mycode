303b53fceff20c353d5f7bcfaefe2f06444be857
Bal Singh <balsinghlearn@gmail.com>
2021-05-10 19:47:06 +0000

    challenge 1

---
+++ lab_input/chal01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print("Please enter your name:")
+user_name = input()
+print("Please enter what day of the week it is:")
+day_of_the_week = input()
+print("Hello {}! Happy {}!".format(user_name,day_of_the_week))
